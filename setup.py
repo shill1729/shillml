8ffa8ff0644d414a8349304a2067c794f2912917
seanhill <seamhill93@gmail.com>
2024-07-23 00:04:18 -0400

    add local cov mse

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def parse_requirements(filename):
 
 setup(
     name='shillml',
-    version='0.0.25',
+    version='0.0.26',
     packages=['shillml'],
     url='https://github.com/shill1729/shillml',
     install_requires=parse_requirements("requirements.txt"),
