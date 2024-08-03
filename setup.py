7da04eb6926c2b4ecd74567e546d1c3d518557d6
seanhill <seamhill93@gmail.com>
2024-08-03 02:27:11 -0400

    plotting additions

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def parse_requirements(filename):
 
 setup(
     name='shillml',
-    version='0.0.54',
+    version='0.0.55',
     packages=['shillml'],
     url='https://github.com/shill1729/shillml',
     install_requires=parse_requirements("requirements.txt"),
