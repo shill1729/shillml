ef9d2b0043fc4a3600cf307c188f6577df26f0bd
seanhill <seamhill93@gmail.com>
2024-08-08 23:49:39 -0400

    major refactor

--- shillml/__init__.py
+++ shillml/__init__.py
@@ -1,9 +1,11 @@
 # shillml/__init__.py
-# from . import diffgeo
-# from . import losses
-# from . import models
-# from . import pdes
-# from . import pointclouds
-# from . import sdes
-# from . import utils
+from . import utils
+from . import sdes
+from . import diffgeo
+from . import models
+from . import losses
+from . import pdes
+from . import pointclouds
+
+
 
